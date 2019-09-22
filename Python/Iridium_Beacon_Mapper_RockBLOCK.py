264ea742fa011b97b2db3f19c141dccfe7114264
Paul <5690545+PaulZC@users.noreply.github.com>
2019-09-22 20:41:43 +0100

    Corrected the Mapper indentation

--- Python/Iridium_Beacon_Mapper_RockBLOCK.py
+++ Python/Iridium_Beacon_Mapper_RockBLOCK.py
@@ -662,10 +662,10 @@ class BeaconMapper(object):
 
    # https://stackoverflow.com/a/2669120
    def sorted_nicely(self, l): 
-    """ Sort the given iterable in the way that humans expect.""" 
-    convert = lambda text: int(text) if text.isdigit() else text 
-    alphanum_key = lambda key: [ convert(c) for c in re.split('([0-9]+)', key) ] 
-    return sorted(l, key = alphanum_key)
+      """ Sort the given iterable in the way that humans expect.""" 
+      convert = lambda text: int(text) if text.isdigit() else text 
+      alphanum_key = lambda key: [ convert(c) for c in re.split('([0-9]+)', key) ] 
+      return sorted(l, key = alphanum_key)
 
    def QUIT(self):
       ''' Quit the program '''
